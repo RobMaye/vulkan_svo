4782943e7fa46c15ea3d4718ceb884d1da95c216
RobMaye <robert.maye@aol.co.uk>
2021-02-18 10:43:26 +0000

    Command buffer recording

--- VulkanEngine/main.cpp
+++ VulkanEngine/main.cpp
@@ -573,6 +573,17 @@ private:
 		if (vkAllocateCommandBuffers(device, &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
 			throw std::runtime_error("failed to allocate command buffers!");
 		}
+
+		for (size_t i = 0; i < commandBuffers.size(); i++) {
+			VkCommandBufferBeginInfo beginInfo{};
+			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+			beginInfo.flags = 0; // Optional
+			beginInfo.pInheritanceInfo = nullptr; // Optional
+
+			if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+				throw std::runtime_error("failed to begin recording command buffer!");
+			}
+		}
 	}
 
 	void createCommandPool() {
